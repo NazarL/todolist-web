fb9096044909e9afe182a444c2e7b9336237cd86
NazarL <nazar.lebedenko@gmail.com>
2021-03-07 17:27:05 -0500

    add mongodb

--- app.js
+++ app.js
@@ -1,25 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const items = ["Walk the dog", "Read a book"];
-const workItems = ["Open Hyper terminal", "$ mkdir Project-name", "$ touch app.js index.html", "$ npm init"];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Item = mongoose.model("Item", {name: String});
+
+const item1 = new Item ({
+    name: "Welcome to your To Do List!"
+});
+
+const item2 = new Item ({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete and item."
+});
+
+const defaultItems = [item1, item2, item3];
 
 //Home route
 app.get("/", function(req, res){
 
-    const day = date.getDate();
+    Item.find({}, function(err, foundItems) {
+        if (err) {
+            console.log(err)
+        } else {
+            //if default items array is 0 -> create items to display
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems, function(err){
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("Documents were added successfully!");
+                    }
+                });
+            } else {
+                //render array items
+                res.render("list", {
+                    listTitle: "Today",
+                    newListItems: foundItems
+                }); 
+            }
+        }
+    })
+
+    //[To-do]: add date 
+    //const day = date.getDate();
 
     //should render all variables simultaneously
-    res.render("list", {
-        listTitle: day,
-        newListItems: items
-    });  
+    // res.render("list", {
+    //     listTitle: "Today",
+    //     newListItems: items
+    // });  
+
+    // res.render("list", {
+    //     listTitle: day,
+    //     newListItems: items
+    // });  
 
 });
 
