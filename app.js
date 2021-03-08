5536a51de03eaac314c514a7bc4e715ce56499bb
NazarL <nazar.lebedenko@gmail.com>
2021-03-08 12:38:26 -0500

    Adding new items to custom lists

--- app.js
+++ app.js
@@ -10,26 +10,25 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Item = mongoose.model("Item", {name: String});
+//List with default items
+const itemsSchema = { name: String};
+const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item ({
-    name: "Welcome to your To Do List!"
-});
-
-const item2 = new Item ({
-    name: "Hit the + button to add a new item."
-});
-
-const item3 = new Item ({
-    name: "<-- Hit this to delete and item."
-});
+const item1 = new Item ({ name: "Welcome to your To Do List!" });
+const item2 = new Item ({ name: "Hit the + button to add a new item." });
+const item3 = new Item ({ name: "<-- Hit this to delete and item." });
 
 const defaultItems = [item1, item2, item3];
+
+//User created list
+const listSchema = { name: String, items: [itemsSchema]};
+const List = mongoose.model("List", listSchema);
 
 //Home route
 app.get("/", function(req, res){
 
     //[To-do]: add date to non work listTitle
+    //find() - gives an array as a result
     Item.find({}, function(err, foundItems) {
         if (err) {
             console.log(err)
@@ -71,23 +70,25 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     const itemName = req.body.newItem;
+    const listName = req.body.list;
 
     const item = new Item({
         name: itemName
     });
 
-    item.save();
-    res.redirect("/");
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({name: listName}, function(err, foundList){
+            if(!err) {
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + listName);
+            }
+        });
 
-    // //check for the correct list to add new item
-    // if (req.body.list === "Work") {
-    //     workItems.push(item);
-    //     res.redirect("/work");
-    // } else {
-    //     items.push(item);
-    //     res.redirect("/");
-    // }
-
+    }
 });
 
 app.post("/delete", function(req, res){
@@ -120,4 +121,33 @@ app.post("/work", function(req, res){
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000.");
+});
+
+//Create dynamic route
+app.get("/:customListName", function(req, res) {
+    const customListName = req.params.customListName;
+
+    //findOne - gives an object back
+    List.findOne({name: customListName}, function(err, foundList) {
+        if(!err) {
+            if(!foundList) {
+                //Create new list
+                const list = new List ({
+                    name: customListName,
+                    items: defaultItems
+                });
+                
+                list.save();
+                res.redirect("/" + customListName);
+
+            } else {
+                //Render existing list
+                res.render("list", {
+                    listTitle: foundList.name,
+                    newListItems: foundList.items
+                });
+            }
+        }
+    });
+
 });
